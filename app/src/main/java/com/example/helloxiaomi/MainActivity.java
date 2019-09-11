65640c783849264c5e97276c0dac457e4261a6be
unknown <drummor@163.com>
2019-09-11 11:09:08 +0800

    first commit

---
+++ app/src/main/java/com/example/helloxiaomi/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.helloxiaomi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //这是一段注释
+        /*
+        这是一段注释你说呢收弄死你是会收到回复粉底是否合适的发f
+        十多年覅i发顺丰你说额你说呢我也不知道怎么回事啊 你说对不睡啊 也yes
+         你说呢 你说呢 79898四年说嗯你说嗯你睡哦呢
+         捏你是皇帝和是否是发sf
+         三年覅是发你说呢你是
+         你属呢
+         纳你是或额
+
+         Synchorzied{}
+         {}
+         */
+    }
+}
